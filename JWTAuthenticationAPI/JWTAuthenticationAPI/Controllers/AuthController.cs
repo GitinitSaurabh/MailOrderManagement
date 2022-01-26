2f4e252deb8f078c8066b734d675d4239859472b
Saurabh <saurabh160196@gmail.com>
2022-01-26 20:34:21 +0530

    adding jwt auth api

---
+++ JWTAuthenticationAPI/JWTAuthenticationAPI/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using JWTAuthenticationAPI.Provider;
+using JWTAuthenticationAPI.Models;
+using Microsoft.Net.Http.Headers;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(AuthController));
+        private readonly IAuthProvider _authProvider;
+
+        public AuthController(IAuthProvider authProvider)
+        {
+            _authProvider = authProvider;
+        }
+
+        [HttpPost]
+        public IActionResult AuthenticateUser(User user)
+        {
+            try
+            {
+                var token = _authProvider.AuthenticateUser(user);
+                if (string.IsNullOrEmpty(token))
+                {
+                    return Unauthorized();
+                }
+                return Ok(token);
+            }
+            catch(Exception exception)
+            {
+                _log4net.Error("Exception found while authenticating the user=" + exception.Message);
+                return new StatusCodeResult(500);
+
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CheckAuthentication()
+        {
+            string headerToken = this.HttpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+            string token = HttpContext.Session.GetString(SessionName);
+            if (String.Equals(token, headerToken))
+            {
+                return Ok();
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ JWTAuthenticationAPI/JWTAuthenticationAPI/Repository/AuthRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTAuthenticationAPI.Models;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace JWTAuthenticationAPI.Repository
+{
+    
+    public class AuthRepository : IAuthRepository
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(AuthRepository));
+        private IConfiguration _config;
+
+        /// <summary>
+        /// Static data for user model is created.
+        /// </summary>
+        private readonly Dictionary<string, string> users = new Dictionary<string, string>() {
+            { "abc","abc"},
+            {"cde","cde" },
+            {"efg","efg" }
+        };
+
+
+        /// <summary>
+        /// Constructor of AuthRepository which initializes IConfiguration Object.
+        /// </summary>
+        /// <param name="config"></param>
+        public AuthRepository(IConfiguration config)
+        {
+            _log4net.Info("AuthenticationRepository constructor initiated.");
+            _config = config;
+        }
+
+        /// <summary>
+        ///  Generates the token if the user credentials are correct.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+
+        public string GenerateToken(User user)
+        {
+            try
+            {
+                _log4net.Info("Token generation started");
+                if (!users.Any(u => u.Key == user.Username && u.Value == user.Password))
+                {
+                    return null;
+                }
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, user.Username.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(
+                        new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return tokenHandler.WriteToken(token);
+            }
+            catch(Exception)
+            {
+                throw ;
+            }
+
+        }
+    }
+}
